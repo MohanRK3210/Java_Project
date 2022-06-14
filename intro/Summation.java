fcd68b5fe4f3aee28d2d5d0d57e67a902af45e22
MohanRK3210 <rkmohanchan1996@gmail.com>
2022-06-14 11:05:46 -0700

    Summation

---
+++ intro/Summation.java
@@ -0,0 +1,14 @@
+class Summation
+{
+	public static void main(String[] args)
+	{
+		// Sum of Two Numbers
+		int a = 5, b = 6, c;
+
+		// Addition of two variables
+		c = a+b; 
+
+		// Printed Statement
+		System.out.println("Sum of two Number :" + a +" + "+ b +" = "+c);
+	}
+}
